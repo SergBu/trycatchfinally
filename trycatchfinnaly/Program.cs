dbd883b7659a21543f5636ec46ddeba9da9d417d
Sergei <sebudnikov@gmail.com>
2024-04-14 23:37:51 +0300

    Добавьте файлы проекта.

---
+++ trycatchfinnaly/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace trycatchfinnaly
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                try
+                {
+                    throw new Exception("Hello from try");
+                }
+                catch
+                {
+                    throw new Exception("Hello from catch");
+                }
+                finally
+                {
+                    throw new Exception("Hello from finally");
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine($"Error message: {ex.Message}");
+            }
+        }
+    }
+}
